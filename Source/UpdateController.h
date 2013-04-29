ed29e1af4efe51ec9d32c3f94f26d1ec3d1fb61c
Mento <Mento@gpgtools.org>
2013-04-29 22:02:42 +0200

    Detect installed version of every tool.

--- Source/UpdateController.h
+++ Source/UpdateController.h
@@ -14,6 +14,7 @@
 	SUUpdater *updater;
 }
 
-
+- (NSString *)pathToTool:(NSString *)tool;
+- (BOOL)isToolInstalled:(NSString *)tool;
 
 @end
--- Source/UpdateController.m
+++ Source/UpdateController.m
@@ -16,19 +16,35 @@
 
 @implementation UpdateController
 @synthesize updater;
-NSDictionary *allOptions;
+NSDictionary *tools; // tools[tool][key]. key is an of: options, path, infoPlist.
 
 
 
+// Init, alloc etc.
 + (void)initialize {
-	NSDictionary *allDomains = @{@"macgpg2" : @"org.gpgtools.macgpg2.updater", @"gpgmail" : @[@"../Containers/com.apple.mail/Data/Library/Preferences/org.gpgtools.gpgmail", @"org.gpgtools.gpgmail"], @"gpgservices" : @"org.gpgtools.gpgservices", @"gka" : @"org.gpgtools.gpgkeychainaccess", @"gpgprefs" : @"org.gpgtools.gpgpreferences"};
-	NSMutableDictionary *tempOptions = [NSMutableDictionary dictionaryWithCapacity:allDomains.count];
-
+#define DKEY @"domain"
+#define PKEY @"path"
+#define IKEY @"identifier"
+	NSDictionary *toolInfos = @{
+							 @"macgpg2" :		@{DKEY : @"org.gpgtools.macgpg2.updater", PKEY : @"/usr/local/MacGPG2/libexec/MacGPG2_Updater.app"},
+		@"gpgmail" :		@{DKEY : @[@"../Containers/com.apple.mail/Data/Library/Preferences/org.gpgtools.gpgmail", @"org.gpgtools.gpgmail"], PKEY : @[@"/Network/Library/Mail/Bundles/GPGMail.mailbundle", @"~/Library/Mail/Bundles/GPGMail.mailbundle", @"/Library/Mail/Bundles/GPGMail.mailbundle"]},
+		@"gpgservices" :	@{DKEY : @"org.gpgtools.gpgservices", PKEY : @[@"~/Library/Services/GPGServices.service", @"/Library/Services/GPGServices.service"]},
+		@"gka" :			@{DKEY : @"org.gpgtools.gpgkeychainaccess", IKEY : @"org.gpgtools.gpgkeychainaccess"},
+		@"gpgprefs" :		@{DKEY : @"org.gpgtools.gpgpreferences", PKEY : @[@"~/Library/PreferencePanes/GPGPreferences.prefPane", @"/Library/PreferencePanes/GPGPreferences.prefPane"]}
+		};
+	
+	NSMutableDictionary *tempTools = [NSMutableDictionary dictionaryWithCapacity:toolInfos.count];
+	
+	
 	NSString *prefDir = [NSHomeDirectory() stringByAppendingString:@"/Library/Preferences/"];
 	NSFileManager *fileManager = [NSFileManager defaultManager];
 	
-	for (NSString *tool in allDomains) {
-		id domains = allDomains[tool]; //NSString or NSArray of NSStrings.
+	for (NSString *tool in toolInfos) {
+		NSMutableDictionary *toolDict = [NSMutableDictionary dictionary];
+		NSDictionary *toolInfo = toolInfos[tool];
+		
+		// GPGOptions for every tool.
+		id domains = toolInfo[DKEY]; //NSString or NSArray of NSStrings.
 		NSString *domain = nil;
 		
 		if ([domains isKindOfClass:[NSArray class]]) {
@@ -50,11 +66,30 @@ NSDictionary *allOptions;
 		GPGOptions *options = [GPGOptions new];
 		options.standardDomain = domain;
 		
-		tempOptions[tool] = options;
+		toolDict[@"options"] = options;
 		[options release];
+		
+		// Paths to the tools.
+		id paths = toolInfo[PKEY];
+		if (!paths) {
+			paths = @[[[NSWorkspace sharedWorkspace] absolutePathForAppBundleWithIdentifier:toolInfo[IKEY]]];
+		} else if (![paths isKindOfClass:[NSArray class]]) {
+			paths = @[paths];
+		}
+		
+		for (NSString *path in paths) {
+			NSString *plistPath = [[path stringByExpandingTildeInPath] stringByAppendingPathComponent:@"Contents/Info.plist"];
+			if ([fileManager fileExistsAtPath:plistPath]) {
+				toolDict[@"path"] = path;
+				toolDict[@"infoPlist"] = [NSDictionary dictionaryWithContentsOfFile:plistPath];
+				break;
+			}
+		}
+		
+		tempTools[tool] = toolDict;
 	}
 	
-	allOptions = [tempOptions copy];
+	tools = [tempTools copy];
 }
 
 - (id)init {
@@ -73,63 +108,14 @@ NSDictionary *allOptions;
 }
 
 
-- (NSString *)feedURLStringForUpdater:(SUUpdater *)updater {
-	NSString *updateSourceKey = @"UpdateSource";
-	NSBundle *bundle = [NSBundle bundleForClass:[self class]];
-	
-	NSString *feedURLKey = @"SUFeedURL";
-	NSString *appcastSource = [[GPGOptions sharedOptions] stringForKey:updateSourceKey];
-	if ([appcastSource isEqualToString:@"nightly"]) {
-		feedURLKey = @"SUFeedURL_nightly";
-	} else if ([appcastSource isEqualToString:@"prerelease"]) {
-		feedURLKey = @"SUFeedURL_prerelease";
-	} else if (![appcastSource isEqualToString:@"stable"]) {
-		NSString *version = [bundle objectForInfoDictionaryKey:@"CFBundleVersion"];
-		if ([version rangeOfCharacterFromSet:[NSCharacterSet characterSetWithCharactersInString:@"nN"]].length > 0) {
-			feedURLKey = @"SUFeedURL_nightly";
-		} else if ([version rangeOfCharacterFromSet:[NSCharacterSet characterSetWithCharactersInString:@"abAB"]].length > 0) {
-			feedURLKey = @"SUFeedURL_prerelease";
-		}
-	}
-	
-	NSString *appcastURL = [bundle objectForInfoDictionaryKey:feedURLKey];
-	if (!appcastURL) {
-		appcastURL = [bundle objectForInfoDictionaryKey:@"SUFeedURL"];
-	}
-	return appcastURL;
-}
 
-- (id)valueForKey:(NSString *)key {
-	if (allOptions[key]) {
-		//Prevent valueForUndefinedKey.
-		return nil;
-	}
-	return [super valueForKey:key];
-}
-
-
-- (GPGOptions *)optionsAndKey:(NSString **)key forKeyPath:(NSString *)keyPath {
-	NSRange range = [keyPath rangeOfString:@"."];
-	if (range.length == 0) {
-		return nil;
-	}
-	NSString *tool = [keyPath substringToIndex:range.location];
-	*key = [keyPath substringFromIndex:range.location + 1];
-	
-	GPGOptions *options = allOptions[tool];
-	if (!options) {
-		return nil;
-	}
-	
-	return options;
-}
-
-
+// Getter and setter.
 - (id)valueForKeyPath:(NSString *)keyPath {
 	NSString *key = nil;
-	GPGOptions *options = [self optionsAndKey:&key forKeyPath:keyPath];
+	NSString *tool = [self toolAndKey:&key forKeyPath:keyPath];
+	GPGOptions *options = tools[tool][@"options"];
 	
-	if (!options) {
+	if (!tool) {
 		return [super valueForKeyPath:keyPath];
 	}
 	
@@ -154,13 +140,25 @@ NSDictionary *allOptions;
 			return @2;
 		}
 		
-		//TODO: Detect installed version!
+		NSDictionary *plist = tools[tool][@"infoPlist"];
+		if (plist) {
+			NSString *version = plist[@"CFBundleVersion"];
+			if ([version rangeOfCharacterFromSet:[NSCharacterSet characterSetWithCharactersInString:@"nN"]].length > 0) {
+				return @2;
+			} else if ([version rangeOfCharacterFromSet:[NSCharacterSet characterSetWithCharactersInString:@"abAB"]].length > 0) {
+				return @1;
+			}
+		}
+		
 		return @0;
+	} else if ([key isEqualToString:@"Installed"]) {
+		return @([self isToolInstalled:tool]);
+	} else if ([key isEqualToString:@"Path"]) {
+		return [self pathToTool:tool];
 	}
 	
 	return [options valueInStandardDefaultsForKey:key];
 }
-
 
 - (void)setValue:(id)value forKeyPath:(NSString *)keyPath {
 	NSString *key = nil;
@@ -205,6 +203,65 @@ NSDictionary *allOptions;
 
 
 
+- (NSString *)pathToTool:(NSString *)tool {
+	return tools[tool][@"path"];
+}
+
+- (BOOL)isToolInstalled:(NSString *)tool {
+	return !!tools[tool][@"path"];
+}
+
+
+
+
+// Helper and other methods.
+- (NSString *)toolAndKey:(NSString **)key forKeyPath:(NSString *)keyPath {
+	NSRange range = [keyPath rangeOfString:@"."];
+	if (range.length == 0) {
+		return nil;
+	}
+	*key = [keyPath substringFromIndex:range.location + 1];
+	
+	return [keyPath substringToIndex:range.location];
+}
+
+- (id)valueForKey:(NSString *)key {
+	if (tools[key]) {
+		//Prevent valueForUndefinedKey.
+		return nil;
+	}
+	return [super valueForKey:key];
+}
+
+
+
+
+// For own Sparkle.
+- (NSString *)feedURLStringForUpdater:(SUUpdater *)updater {
+	NSString *updateSourceKey = @"UpdateSource";
+	NSBundle *bundle = [NSBundle bundleForClass:[self class]];
+	
+	NSString *feedURLKey = @"SUFeedURL";
+	NSString *appcastSource = [[GPGOptions sharedOptions] stringForKey:updateSourceKey];
+	if ([appcastSource isEqualToString:@"nightly"]) {
+		feedURLKey = @"SUFeedURL_nightly";
+	} else if ([appcastSource isEqualToString:@"prerelease"]) {
+		feedURLKey = @"SUFeedURL_prerelease";
+	} else if (![appcastSource isEqualToString:@"stable"]) {
+		NSString *version = [bundle objectForInfoDictionaryKey:@"CFBundleVersion"];
+		if ([version rangeOfCharacterFromSet:[NSCharacterSet characterSetWithCharactersInString:@"nN"]].length > 0) {
+			feedURLKey = @"SUFeedURL_nightly";
+		} else if ([version rangeOfCharacterFromSet:[NSCharacterSet characterSetWithCharactersInString:@"abAB"]].length > 0) {
+			feedURLKey = @"SUFeedURL_prerelease";
+		}
+	}
+	
+	NSString *appcastURL = [bundle objectForInfoDictionaryKey:feedURLKey];
+	if (!appcastURL) {
+		appcastURL = [bundle objectForInfoDictionaryKey:@"SUFeedURL"];
+	}
+	return appcastURL;
+}
 
 
 
