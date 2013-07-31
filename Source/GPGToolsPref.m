00e808fc619154dfd58e1963058342bdc9a1e005
Mento <Mento@gpgtools.org>
2013-07-31 16:49:20 +0200

    Code sign check

--- Source/GPGToolsPref.m
+++ Source/GPGToolsPref.m
@@ -8,29 +8,22 @@
 
 #import "GPGToolsPref.h"
 #import <Libmacgpg/Libmacgpg.h>
-#import "NSBundle+Sandbox.h"
 
 @implementation GPGToolsPref
-
-+ (void)initialize {
-#ifndef DEBUG
-    // Check the validity of the code signature.
-    OBCodeSignState codeSignState = [NSBundle bundleForClass:[self class].ob_codeSignState;
-    if(codeSignState != OBCodeSignStateSignatureValid && codeSignState != OBCodeSignStateUnsigned) {
-        NSRunAlertPanel(@"Someone tampered with your installation of GPGPreferences!", @"To keep you safe, GPGPreferences will not be loaded!\n\nPlease download and install the latest version of GPG Suite from https://gpgtools.org to be sure you have an original version from us!", @"", nil, nil, nil);
-        exit(1);
-    }
-#endif
-}
 
 - (NSString *)mainNibName {
 	if (![GPGController class]) {
 		return @"WarningView";
 	}
+#ifdef CODE_SIGN_CHECK
+	/* Check the validity of the code signature. */
+    if (![NSBundle bundleForClass:[self class]].isValidSigned) {
+		NSRunAlertPanel(@"Someone tampered with your installation of GPGPreferences!",
+						@"To keep you safe, GPGPreferences will not be loaded!\n\nPlease download and install the latest version of GPG Suite from https://gpgtools.org to be sure you have an original version from us!", nil, nil, nil);
+        exit(1);
+    }
+#endif
 	return [super mainNibName];
-}
-
-- (void) mainViewDidLoad {
 }
 
 @end
