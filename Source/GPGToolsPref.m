583ba899f72814715166582fd3fbd4de83738f26
Lukas Pitschl <lukas@dressyvagabonds.com>
2013-08-03 01:31:00 +0200

    Use isValidSigned from Libmacgpg.

--- Source/GPGToolsPref.m
+++ Source/GPGToolsPref.m
@@ -8,14 +8,13 @@
 
 #import "GPGToolsPref.h"
 #import <Libmacgpg/Libmacgpg.h>
-#import "NSBundle+Sandbox.h"
 
 @implementation GPGToolsPref
 
 + (void)initialize {
-#ifndef DEBUG
+#ifdef CODE_SIGN_CHECK
     // Check the validity of the code signature.
-    if([[NSBundle bundleForClass:[self class]] ob_codeSignState] != OBCodeSignStateSignatureValid) {
+    if([[NSBundle bundleForClass:[self class]] isValidSigned]) {
         NSRunAlertPanel(@"Someone tampered with your installation of GPGPreferences!", @"To keep you safe, GPGPreferences will not be loaded!\n\nPlease download and install the latest version of GPG Suite from https://gpgtools.org to be sure you have an original version from us!", @"", nil, nil, nil);
         exit(1);
     }
