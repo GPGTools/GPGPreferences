85fa0402c5565af63194cb04e8bf1675e39d6ddc
Mento <Mento@gpgtools.org>
2014-06-22 18:06:53 +0200

    Lazy Sparkle loading.

--- Source/UpdateController.m
+++ Source/UpdateController.m
@@ -10,7 +10,7 @@
 #import "UpdateController.h"
 
 @interface UpdateController()
-@property (strong) SUUpdater *updater;
+@property (strong) id /*SUUpdater **/updater;
 @property (unsafe_unretained) NSBundle *bundle;
 @end
 
@@ -110,11 +110,28 @@ NSMutableDictionary *tools;
 		return nil;
 	}
 	self.bundle = [NSBundle bundleForClass:[self class]];
-	self.updater = [SUUpdater updaterForBundle:self.bundle];
-	updater.delegate = self;
+	
+	Class sparkleClass = NSClassFromString(@"SUUpdater");
+	
+	if (!sparkleClass) {
+		NSString *sparklePath = [self.bundle.bundlePath stringByAppendingPathComponent:@"Contents/Frameworks/Sparkle.framework"];
+		NSBundle *sparkleBundle = [NSBundle bundleWithPath:sparklePath];
+		NSError *error = nil;
+		
+		if (![sparkleBundle loadAndReturnError:&error]) {
+			NSLog(@"Unable to laod Sparkle: %@", error);
+		}
+		sparkleClass = NSClassFromString(@"SUUpdater");
+	}
+	
+	self.updater = [sparkleClass updaterForBundle:self.bundle];
+	[updater setDelegate:self];
+
 	
 	return self;
 }
+	 
+	 
 
 
 // Getter and setter.
